155a3803695035c2417989d52b79d5177d36adc6
Reese Whitlock <reesekwhitlock@gmail.com>
2026-06-07 16:21:23 -0700

    use vlan instead

--- build.sh
+++ build.sh
@@ -12,4 +12,4 @@ NODE_NAME=${2^^}
 
 echo "Building and running container $CONTAINER_NAME on node $NODE_NAME"
 
-sudo podman run -d --replace --name counter-"$CONTAINER_NAME" --network host udp-counter node "$NODE_NAME" 9000 10
+sudo podman run -d --replace --name counter-"$CONTAINER_NAME" --network vlan udp-counter node "$NODE_NAME" 9000 10
