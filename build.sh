18f7b53dd528a393dbbe275b1605cfde29ab1f53
Reese Whitlock <reesekwhitlock@gmail.com>
2026-06-07 16:31:59 -0700

    container ip based on container name

--- build.sh
+++ build.sh
@@ -10,6 +10,15 @@ sudo podman build -t udp-counter -f Containerfile .
 CONTAINER_NAME=$1
 NODE_NAME=${2^^}
 
-echo "Building and running container $CONTAINER_NAME on node $NODE_NAME"
+# assign ip based on container name
+# e.g. counter-a -> 10.24.24.10
+#      counter-b -> 10.24.24.11, etc.
 
-sudo podman run -d --replace --name counter-"$CONTAINER_NAME" --network vlan udp-counter node "$NODE_NAME" 9000 10
+CHAR_LOWER=$(echo "${CONTAINER_NAME:0:1}" | tr '[:upper:]' '[:lower:]')
+ASCII_VAL=$(printf "%d" "'$CHAR_LOWER")
+IP_SUFFIX=$((ASCII_VAL - 97 + 10))
+IP="10.24.24.$IP_SUFFIX"
+
+echo "Building and running container $CONTAINER_NAME on node $NODE_NAME with IP $IP"
+
+sudo podman run -d --replace --name counter-"$CONTAINER_NAME" --network vlan:ip=$IP udp-counter node "$NODE_NAME" 9000 10
